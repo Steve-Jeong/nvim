bcabf3553660ed36e934ddcd281ef8fd03b5ff99
Steve Jeong <jst0930@gmail.com>
2025-07-26 14:17:13 +0300

    telescope.lua 기능 추가

--- lua/plugins/spec/telescope.lua
+++ lua/plugins/spec/telescope.lua
@@ -4,7 +4,11 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<C-p>",     "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live grep" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>",  desc = "Old Files" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",  desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help Tags" },
   },
   config = function()
     -- Telescope의 추가적인 설정이 필요하면 여기에 작성합니다.
