4ce6df66237fa5be410ee4f40be2bb6f61c46496
Steve Jeong <jst0930@gmail.com>
2025-07-26 07:12:20 +0300

    catppuccine available color scheme marked

--- lua/plugins/spec/colorscheme.lua
+++ lua/plugins/spec/colorscheme.lua
@@ -3,7 +3,10 @@ return {
   name = "catppuccin",
   priority = 1000, -- 다른 플러그인보다 먼저 로드되도록 우선순위 설정
   config = function()
-    require("catppuccin").setup()
+    require("catppuccin").setup({
+       -- available color scheme : latte, frappe, macchiato, mocha
+       flavour = "macchiato"
+    })
     vim.cmd.colorscheme("catppuccin")
   end,
 }
